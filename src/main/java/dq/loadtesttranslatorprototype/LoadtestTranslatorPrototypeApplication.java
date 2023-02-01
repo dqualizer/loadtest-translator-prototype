a62213d74be383c24c175cc15463ff2b0706dac4
EddeCCC <eduard.schander@gmx.de>
2023-02-01 16:39:23 +0100

    first commit

---
+++ src/main/java/dq/loadtesttranslatorprototype/LoadtestTranslatorPrototypeApplication.java
@@ -0,0 +1,13 @@
+package dq.loadtesttranslatorprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadtestTranslatorPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadtestTranslatorPrototypeApplication.class, args);
+		System.out.println("### PROTOTYPE STARTED ###");
+	}
+}
---
+++ src/test/java/dq/loadtesttranslatorprototype/LoadtestTranslatorPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package dq.loadtesttranslatorprototype;
+
+import org.junit.jupiter.api.Test;
+
+class LoadtestTranslatorPrototypeApplicationTests {
+
+	/**
+	 * No tests available yet
+	 */
+	@Test
+	void contextLoads() {
+	}
+}
