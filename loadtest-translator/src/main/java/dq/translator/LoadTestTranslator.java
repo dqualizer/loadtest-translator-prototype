4e81ade03f9a28c6d51643a0c2a5fb5af2c0e4c4
EddeCCC <eduard.schander@gmx.de>
2023-02-06 08:19:51 +0100

    remove ConstantReplacer

--- loadtest-translator/src/main/java/dq/translator/LoadTestTranslator.java
+++ loadtest-translator/src/main/java/dq/translator/LoadTestTranslator.java
@@ -7,7 +7,6 @@ import dq.dqlang.modeling.*;
 import dq.exception.EnvironmentNotFoundException;
 import dq.exception.IDNotFoundException;
 import dq.mock.URLRetrieverMock;
-import dq.util.ConstantReplacer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
